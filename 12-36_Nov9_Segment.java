a90b22568cdcf1db8453bca8e0f21949efb9687a
Greg Bischoff <gsbischoff@gmail.com>
2018-11-09 12:38:17 -0500

    Made changes to segment, intersection

---
+++ 12-36_Nov9_Segment.java
@@ -0,0 +1,69 @@
+// Programmer: FOXES (Greg, Lauren, Maxine) (c) Copyright 2018
+// *****************************************************************************
+// *****************************************************************************
+// **** Segment
+// *****************************************************************************
+// *****************************************************************************
+
+public class Segment{
+  private Queue<Car> myCarQueue;
+
+  private int myCapacity;
+  private int myDirection;
+
+  private boolean isEdge;
+
+  public Segment(int capacity, int direction) {
+    myCapacity = capacity;
+    myDirection = direction;
+    isEdge = false;
+
+    myCarQueue = new Queue<Car>();
+  }//end of Segment Constructor
+
+  public void setIsEdge(boolean b){
+    isEdge = b;
+    return;
+  }//setIsEdge
+
+  public boolean getIsEdge(){
+    return isEdge;
+  }//getIsEdge
+
+  public boolean putCar(Car car)
+  {
+    if(myCarQueue.size() < myCapacity)
+    {
+        myCarQueue.add(car);
+        return true;
+    } // end of if(myCarQueue.size() < myCapacity)
+    return false;
+  }
+
+  public boolean putNewCar()
+  {
+    Car car = new Car();
+    return putCar(car);
+  } // end of putNewCar()
+
+  public Car getHeadCar()
+  {
+    return myCarQueue.peek();
+  } // end of getHeadCar()
+
+  public Car removeHeadCar()
+  {
+    return myCarQueue.poll();
+  } // end of removeHeadCar()
+
+  public int capacity()
+  {
+      return myCapacity;
+  } // end of capacity()
+
+  public string getCarsInfo()
+  {
+
+  } // end of getCarsInfo()
+
+}//end of Segment
