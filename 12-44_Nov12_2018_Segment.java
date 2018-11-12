37a947e9375ed9b4a142e5411207a2b9b4342afd
Greg Bischoff <gsbischoff@gmail.com>
2018-11-12 12:45:27 -0500

    Iterative

---
+++ 12-44_Nov12_2018_Segment.java
@@ -0,0 +1,72 @@
+// Programmer: FOXES (Greg, Lauren, Maxine) (c) Copyright 2018
+// *****************************************************************************
+// *****************************************************************************
+// **** Segment
+// *****************************************************************************
+// *****************************************************************************
+import java.util.*;
+public class Segment{
+  private LinkedList<Car> myCarQueue;
+
+  private int myCapacity;
+  private int myDirection;
+
+  private boolean isEdge;
+
+  public Segment(int capacity, int direction){
+    myCapacity = capacity;
+    myDirection = direction;
+    isEdge = false;
+
+    myCarQueue = new LinkedList<Car>();
+  } // end of Segment Constructor
+
+  public void setIsEdge(boolean b){
+    isEdge = b;
+    return;
+  }// end of setIsEdge
+
+  public boolean getIsEdge(){
+    return isEdge;
+  }// end of getIsEdge
+
+  public boolean putCar(Car car){
+    if(myCarQueue.size() < myCapacity){
+        myCarQueue.add(car);
+        return true;
+    } // end of if(myCarQueue.size() < myCapacity)
+    return false;
+  } // end of putCar
+
+  public Car getHeadCar(){
+    return myCarQueue.peek();
+  } // end of getHeadCar
+
+  public Car removeHeadCar(){
+    return myCarQueue.poll();
+  } // end of removeHeadCar
+
+  public boolean isFull(){
+    if (myCarQueue.size() == myCapacity){
+      return true;
+    } // end of if (the capacity of the Segment instance is reached)
+    return false;
+  } // end of isFull
+
+  public int capacity(){
+      return myCapacity;
+  } // end of capacity
+
+  public boolean isEmpty(){
+    if(myCarQueue.size() > 0){
+      return false;
+    } // end of if(myCarQueue.size() > 0)
+
+      return true;
+  } // end of isEmpty
+
+  public String getCarsInfo(){
+    return "";
+  } // end of getCarsInfo()
+
+} // end of Segment
