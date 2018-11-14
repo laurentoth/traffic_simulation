571c3324dbf3cbdd78e7770e6ecc84f854d11d6f
Lauren Toth <laurentoth@Laurens-MacBook-Pro.local>
2018-11-14 12:56:50 -0500

    Got rid of capacity

--- 12-55_Nov14_2018_GridControl.java
+++ 12-55_Nov14_2018_GridControl.java
@@ -65,7 +65,7 @@ public class GridControl{
             intersection[row + 1][col].getInbound(N), N);
         }
         else{
-          Segment sg = new Segment(N);
+          Segment sg = new Segment(SEGMENT_CAPACITY, N);
           sg.setIsEdge(true);
           intersection[row][col].setOutbound(sg, N);
         } // end of if(row + 1 < numIntersections) else ...
@@ -75,7 +75,7 @@ public class GridControl{
             intersection[row][col - 1].getInbound(W), W);
         }
         else{
-          Segment sg = new Segment(W);
+          Segment sg = new Segment(SEGMENT_CAPACITY, W);
           sg.setIsEdge(true);
           intersection[row][col].setOutbound(sg, W);
         } // end of if(col - 1 > 0) else ...
@@ -85,7 +85,7 @@ public class GridControl{
             intersection[row - 1][col].getInbound(S), S);
         }
         else {
-          Segment sg = new Segment(S);
+          Segment sg = new Segment(SEGMENT_CAPACITY, S);
           sg.setIsEdge(true);
           intersection[row][col].setOutbound(sg, S);
         } // end of if(row - 1 > 0) else ...
@@ -95,7 +95,7 @@ public class GridControl{
             intersection[row][col + 1].getInbound(E), E);
         }
         else {
-          Segment sg = new Segment(E);
+          Segment sg = new Segment(SEGMENT_CAPACITY, E);
           sg.setIsEdge(true);
           intersection[row][col].setOutbound(sg, E);
         } // end of if(col + 1 < numIntersections) else... 
--- Segment.java
+++ Segment.java
@@ -9,13 +9,11 @@ public class Segment{
 
   private Queue<Car> myCarQueue;
 
-  private int myCapacity;
   private int myDirection;
 
   private boolean isEdge;
 
-  public Segment(int capacity, int direction){
-    myCapacity = capacity;
+  public Segment(int direction){
     myDirection = direction;
     isEdge = false;
 
@@ -32,11 +30,8 @@ public class Segment{
   }// end of getIsEdge
 
   public boolean putCar(Car car){
-    if(myCarQueue.size() < myCapacity){
         myCarQueue.add(car);
         return true;
-    } // end of if(myCarQueue.size() < myCapacity)
-    return false;
   } // end of putCar
 
   public void advanceCarTimeOnGrid(){
@@ -54,13 +49,15 @@ public class Segment{
     return myCarQueue.poll();
   } // end of removeHeadCar
 
+  /* for future use
   public boolean isFull(){
     if(myCarQueue.size() == myCapacity){
       return true;
     } // end of if(the capacity of the Segment instance is reached)
     return false;
   } // end of isFull
-
+ */
+  
   public int capacity(){
       return myCapacity;
   } // end of capacity
